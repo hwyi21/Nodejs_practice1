4c012e3fdfa73161d32b0c7f74bde9c634b035a6
hwyi21 <chozza2019@naver.com>
2020-07-22 10:52:01 +0900

    Node.js 연습

---
+++ myobj.js
@@ -0,0 +1,15 @@
+/*
+
+모듈 속에 메서드 정의시에 아래와 같이한다.
+*/
+exports.getMsg=function(){
+	return "hi";
+}
+
+exports.sum=function(a,b){
+	return a+b;
+}
+
+exports.multi=function(a,b){
+	return a*b;
+}
---
+++ process.js
@@ -0,0 +1,7 @@
+/*
+process는 시스템 정보와 관련된 객체
+env : 환경변수 출력
+*/
+setInterval(function(){
+	console.log("test");
+}, 1000);
